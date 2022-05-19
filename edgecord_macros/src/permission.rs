15df0ad1f66c80cc8983979001d76d9a37549690
sizumita <sumito@izumita.com>
2022-05-19 23:41:09 +0900

    Improve PermissionFlagBits

--- edgecord_macros/src/permission.rs
+++ edgecord_macros/src/permission.rs
@@ -2,13 +2,13 @@ use darling::util::Flag;
 use twilight_model::guild::Permissions;
 
 macro_rules! flag_bits {
-    (struct $name:ident { $($fname:ident : $ftype:ty),* }) => {
+    ($($fname:ident),*) => {
         #[derive(Default, Debug, darling::FromMeta)]
-        pub struct $name {
-            pub $($fname : $ftype),*
+        pub struct PermissionFlagBits {
+            pub $($fname : Flag),*
         }
 
-        impl $name {
+        impl PermissionFlagBits {
             pub fn bits(&self) -> Permissions {
                 macro_rules! flag {
                     ($flag_name:ident) => {
@@ -27,46 +27,44 @@ macro_rules! flag_bits {
 }
 
 flag_bits! {
-    struct PermissionFlagBits {
-        create_invite: Flag,
-        kick_members: Flag,
-        ban_members: Flag,
-        administrator: Flag,
-        manage_channels: Flag,
-        manage_guild: Flag,
-        add_reactions: Flag,
-        view_audit_log: Flag,
-        priority_speaker: Flag,
-        stream: Flag,
-        view_channel: Flag,
-        send_messages: Flag,
-        send_tts_messages: Flag,
-        manage_messages: Flag,
-        embed_links: Flag,
-        attach_files: Flag,
-        read_message_history: Flag,
-        mention_everyone: Flag,
-        use_external_emojis: Flag,
-        view_guild_insights: Flag,
-        connect: Flag,
-        speak: Flag,
-        mute_members: Flag,
-        deafen_members: Flag,
-        move_members: Flag,
-        use_vad: Flag,
-        change_nickname: Flag,
-        manage_nicknames: Flag,
-        manage_roles: Flag,
-        manage_webhooks: Flag,
-        manage_emojis_and_stickers: Flag,
-        request_to_speak: Flag,
-        manage_events: Flag,
-        manage_threads: Flag,
-        create_public_threads: Flag,
-        create_private_threads: Flag,
-        use_external_stickers: Flag,
-        send_messages_in_threads: Flag,
-        use_embedded_activities: Flag,
-        moderate_members: Flag
-    }
+    create_invite,
+    kick_members,
+    ban_members,
+    administrator,
+    manage_channels,
+    manage_guild,
+    add_reactions,
+    view_audit_log,
+    priority_speaker,
+    stream,
+    view_channel,
+    send_messages,
+    send_tts_messages,
+    manage_messages,
+    embed_links,
+    attach_files,
+    read_message_history,
+    mention_everyone,
+    use_external_emojis,
+    view_guild_insights,
+    connect,
+    speak,
+    mute_members,
+    deafen_members,
+    move_members,
+    use_vad,
+    change_nickname,
+    manage_nicknames,
+    manage_roles,
+    manage_webhooks,
+    manage_emojis_and_stickers,
+    request_to_speak,
+    manage_events,
+    manage_threads,
+    create_public_threads,
+    create_private_threads,
+    use_external_stickers,
+    send_messages_in_threads,
+    use_embedded_activities,
+    moderate_members
 }
